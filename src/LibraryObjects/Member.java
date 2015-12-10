d64e087d189e91e33a70c14bfaa3cd7aed78625b
suryadev <suryadev@192.168.1.35>
2015-12-10 16:16:59 +0530

    First

---
+++ src/LibraryObjects/Member.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LibraryObjects;
+
+/**
+ *
+ * @author suryadev
+ */
+public class Member {
+    
+}
